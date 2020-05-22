2340e6e244cf89449d53ebeeffc25c6f8929af4a
Joe Kemp <joe@close.io>
2020-05-22 11:32:24 -0400

    Allow up to pymongo 3.7

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(name='mongoengine',
       long_description=LONG_DESCRIPTION,
       platforms=['any'],
       classifiers=CLASSIFIERS,
-      install_requires=['pymongo>=2.8,<3.5'],
+      install_requires=['pymongo>=2.8,<3.8'],
       test_suite='nose.collector',
       **extra_opts
 )
